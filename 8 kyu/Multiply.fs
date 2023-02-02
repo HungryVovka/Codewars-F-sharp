c73002a2a9a3474ef055a99722b066c203bcf698
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-02-02 22:13:00 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Multiply.fs
@@ -0,0 +1,7 @@
+// -----------------------------------------------------------
+// This code does not execute properly. Try to figure out why.
+// -----------------------------------------------------------
+
+module Multiply.Solution
+
+let multiply a b = a * b;;
