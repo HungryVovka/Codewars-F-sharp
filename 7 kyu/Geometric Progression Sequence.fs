66535a0fbee3baad18241380d23e06a6831de77c
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-04-06 01:10:59 +0500

    feat: 7 kyu solutions

--- 7 kyu/Geometric Progression Sequence.fs
+++ 7 kyu/Geometric Progression Sequence.fs
@@ -23,3 +23,24 @@ let geometricSequenceElements (a: int)(r: int)(n: int) =
     |> List.scan (fun x _ -> r * x) a
     |> List.map string
     |> String.concat ", ";;
+
+// -----------------------------------------------------------
+// License
+// Tasks are the property of Codewars (https://www.codewars.com/) 
+// and users of this resource.
+// 
+// All solution code in this repository 
+// is the personal property of Vladimir Rukavishnikov
+// (vladimirrukavishnikovmail@gmail.com).
+// 
+// Copyright (C) 2023 Vladimir Rukavishnikov
+// 
+// This file is part of the HungryVovka/Codewars-F-sharp
+// (https://github.com/HungryVovka/Codewars-F-sharp)
+// 
+// License is GNU General Public License v3.0
+// (https://github.com/HungryVovka/Codewars-Fsharp/blob/main/LICENSE)
+// 
+// You should have received a copy of the GNU General Public License v3.0
+// along with this code. If not, see http://www.gnu.org/licenses/
+// -----------------------------------------------------------
--- 7 kyu/List Filtering.fs
+++ 7 kyu/List Filtering.fs
@@ -22,3 +22,24 @@ open System
 
 let GetIntegersFromList (listOfItems: Object list) =
     [for i in listOfItems do if i :? int then yield i :?> int];;
+
+// -----------------------------------------------------------
+// License
+// Tasks are the property of Codewars (https://www.codewars.com/) 
+// and users of this resource.
+// 
+// All solution code in this repository 
+// is the personal property of Vladimir Rukavishnikov
+// (vladimirrukavishnikovmail@gmail.com).
+// 
+// Copyright (C) 2023 Vladimir Rukavishnikov
+// 
+// This file is part of the HungryVovka/Codewars-F-sharp
+// (https://github.com/HungryVovka/Codewars-F-sharp)
+// 
+// License is GNU General Public License v3.0
+// (https://github.com/HungryVovka/Codewars-Fsharp/blob/main/LICENSE)
+// 
+// You should have received a copy of the GNU General Public License v3.0
+// along with this code. If not, see http://www.gnu.org/licenses/
+// -----------------------------------------------------------
