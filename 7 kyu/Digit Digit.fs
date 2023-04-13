c15e6e3da6c1ddd1cfd7362929f558259ba4f91e
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-04-14 00:27:36 +0500

    feat: 7 kyu solutions

---
+++ 7 kyu/Digit Digit.fs
@@ -0,0 +1,43 @@
+// -----------------------------------------------------------
+// Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+// 
+// For example, if we run 9119 through the function, 811181 will come out, because 9^2 is 81 and 1^2 is 1. (81-1-1-81)
+// 
+// Example #2: An input of 765 will/should return 493625 because 7^2 is 49, 6^2 is 36, and 5^2 is 25. (49-36-25)
+// 
+// Note: The function accepts an integer and returns an integer.
+// 
+// Happy Coding!
+// -----------------------------------------------------------
+
+module SquareEveryDigit.Solution
+
+open System
+
+let squareDigits num =
+    (string num).ToCharArray()
+    |> Array.map (fun n -> float(n.ToString())**2.0)
+    |> Array.map string
+    |> String.concat ""
+    |> int;;
+
+// -----------------------------------------------------------
+// License
+// Tasks are the property of Codewars (https://www.codewars.com/) 
+// and users of this resource.
+// 
+// All solution code in this repository 
+// is the personal property of Vladimir Rukavishnikov
+// (vladimirrukavishnikovmail@gmail.com).
+// 
+// Copyright (C) 2023 Vladimir Rukavishnikov
+// 
+// This file is part of the HungryVovka/Codewars-F-sharp
+// (https://github.com/HungryVovka/Codewars-F-sharp)
+// 
+// License is GNU General Public License v3.0
+// (https://github.com/HungryVovka/Codewars-Fsharp/blob/main/LICENSE)
+// 
+// You should have received a copy of the GNU General Public License v3.0
+// along with this code. If not, see http://www.gnu.org/licenses/
+// -----------------------------------------------------------
