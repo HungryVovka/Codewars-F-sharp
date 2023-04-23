66f8b8413b57a26fa7089e0b15d03ec159f485d3
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-04-23 22:13:06 +0500

    feat: 7 kyu solutions

--- 7 kyu/Isograms.fs
+++ 7 kyu/Isograms.fs
@@ -56,7 +56,7 @@ let isIsogram (str : string) =
 // (https://github.com/HungryVovka/Codewars-F-sharp)
 // 
 // License is GNU General Public License v3.0
-// (https://github.com/HungryVovka/Codewars-Fsharp/blob/main/LICENSE)
+// (https://github.com/HungryVovka/Codewars-F-sharp/blob/main/LICENSE)
 // 
 // You should have received a copy of the GNU General Public License v3.0
 // along with this code. If not, see http://www.gnu.org/licenses/
--- 7 kyu/List Filtering.fs
+++ 7 kyu/List Filtering.fs
@@ -38,7 +38,7 @@ let GetIntegersFromList (listOfItems: Object list) =
 // (https://github.com/HungryVovka/Codewars-F-sharp)
 // 
 // License is GNU General Public License v3.0
-// (https://github.com/HungryVovka/Codewars-Fsharp/blob/main/LICENSE)
+// (https://github.com/HungryVovka/Codewars-F-sharp/blob/main/LICENSE)
 // 
 // You should have received a copy of the GNU General Public License v3.0
 // along with this code. If not, see http://www.gnu.org/licenses/
