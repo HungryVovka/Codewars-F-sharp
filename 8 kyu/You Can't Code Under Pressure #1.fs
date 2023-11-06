13843d979e7b174ad43853ae12612ea16776ba44
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-11-06 23:02:50 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/You Can't Code Under Pressure #1.fs
@@ -0,0 +1,34 @@
+// -----------------------------------------------------------
+// Code as fast as you can! You need to double the integer and return it.
+// -----------------------------------------------------------
+
+module Kata
+
+let doubleInteger (n: int): int = n * 2;;
+
+// or
+
+module Kata 
+
+let doubleInteger = (*) 2;;
+
+// -----------------------------------------------------------
+// License
+// Tasks are the property of Codewars (https://www.codewars.com/) 
+// and users of this resource.
+// 
+// All solution code in this repository 
+// is the personal property of Vladimir Rukavishnikov
+// (vladimirrukavishnikovmail@gmail.com).
+// 
+// Copyright (C) 2023 Vladimir Rukavishnikov
+// 
+// This file is part of the HungryVovka/Codewars-F-sharp
+// (https://github.com/HungryVovka/Codewars-F-sharp)
+// 
+// License is GNU General Public License v3.0
+// (https://github.com/HungryVovka/Codewars-F-sharp/blob/main/LICENSE)
+// 
+// You should have received a copy of the GNU General Public License v3.0
+// along with this code. If not, see http://www.gnu.org/licenses/
+// -----------------------------------------------------------
