e70b167d55212fd2981510830c683bb49fb060ba
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-11-06 22:52:40 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Even or Odd.fs
@@ -0,0 +1,31 @@
+// -----------------------------------------------------------
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+// -----------------------------------------------------------
+
+module KataModule
+
+let evenOrOdd (n: int): string =
+	match (n % 2) with
+	| 0 -> "Even"
+	|_ -> "Odd";;
+
+// -----------------------------------------------------------
+// License
+// Tasks are the property of Codewars (https://www.codewars.com/) 
+// and users of this resource.
+// 
+// All solution code in this repository 
+// is the personal property of Vladimir Rukavishnikov
+// (vladimirrukavishnikovmail@gmail.com).
+// 
+// Copyright (C) 2023 Vladimir Rukavishnikov
+// 
+// This file is part of the HungryVovka/Codewars-F-sharp
+// (https://github.com/HungryVovka/Codewars-F-sharp)
+// 
+// License is GNU General Public License v3.0
+// (https://github.com/HungryVovka/Codewars-F-sharp/blob/main/LICENSE)
+// 
+// You should have received a copy of the GNU General Public License v3.0
+// along with this code. If not, see http://www.gnu.org/licenses/
+// -----------------------------------------------------------
